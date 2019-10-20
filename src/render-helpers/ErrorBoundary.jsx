626aa2f74a19dd03fcf0c58d5e23d58b03b8d75e
Anthony May <anthonymay@anthonys-mbp.lan>
2019-10-20 16:54:32 +1100

    Debugging error boundary

--- src/render-helpers/ErrorBoundary.jsx
+++ src/render-helpers/ErrorBoundary.jsx
@@ -19,7 +19,7 @@ export default class ErrorBoundary extends React.Component {
   render() {
     if (this.state.hasError) {
       // You can render any custom fallback UI
-      return <h1>Something went wrong.</h1>;
+      return <>Something went wrong.</>;
     }
 
     return this.props.children;
